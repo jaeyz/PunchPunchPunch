0ff8404295772e96433e89f56662c545a7dfc3a1
cii7 <njaeyz@gmail.com>
2015-01-11 04:26:08 -0800

    Loading Screen

--- PunchPunchPunch/Assets/Scripts/LoadingScreenController.cs
+++ PunchPunchPunch/Assets/Scripts/LoadingScreenController.cs
@@ -9,12 +9,16 @@ public class LoadingScreenController : MonoBehaviour {
 	[SerializeField]
 	private CPButton button;
 
+	[SerializeField]
+	private GameObject tapToStart;
+
 	// Use this for initialization
 	void Start () {
 		ScreenSizeSupport.Instance.root = GetComponent<UIRoot> ();
 		panel.localScale = ScreenSizeSupport.Instance.GetPrefabScale ();
 		SoundManager.Instance.PlaySound (Sounds.MAIN_CLIP, true);
 		button.AddListeners (OnStartClick);
+		iTween.FadeFrom (tapToStart, iTween.Hash("alpha", 0, "time", 1f, "looptype", iTween.LoopType.loop));
 	}
 	
 	void OnDestroy() {
